4a3aa774b3852b63d1ee232816b1a6cbbe74c847
Alekz <52288284+MrAlekzAedrix@users.noreply.github.com>
2019-07-22 17:35:59 -0500

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,14 @@
+﻿from lineal import *
+from numpy import *
+#Fila
+a=array([[4, 2, 5], [2, 5, 8], [2,4,3]], float)
+#print(a)
+#print("")
+
+#Definición del vector constantes
+#Columna
+b=array([[18], [27.3], [16.2]], float)
+#print(b)
+#print("")
+
+print (gauss(a,b))
