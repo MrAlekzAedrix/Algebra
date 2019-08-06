38fff9c5788852cf170297c6694529827d070a42
Alekz <52288284+MrAlekzAedrix@users.noreply.github.com>
2019-08-06 17:02:00 -0500

    Add files via upload

---
+++ test5.py
@@ -0,0 +1,7 @@
+from numpy import *
+from lineal import *
+a=array([[5,-3,1],[2,4,-1],[2,-3,8]], float)
+b=array([[5],[6],[4]], float)
+x=array([[1],[1],[1]], float)
+[x]=jacobi1(a,b,x);print(x)
+print(k)
---
+++ test7.py
@@ -0,0 +1,10 @@
+from numpy import *
+from lineal import *
+a=array([[5,-3,1],[2,4,-1],[2,-3,8]], float)
+b=array([[5],[6],[4]], float)
+x=array([[1],[1],[1]], float)
+x=gaussSeidel(a,b,x);print(x)
+x=gaussSeidel(a,b,x);print(x)
+x=gaussSeidel(a,b,x);print(x)
+x=gaussSeidel(a,b,x);print(x)
+x=gaussSeidel(a,b,x);print(x)
